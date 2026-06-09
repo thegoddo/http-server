39c2433ce24ec249238e84b81a0ab2320d84d865
theGoddo <biswajitshaw220@gmail.com>
2026-06-09 13:49:25 +0530

    server complete

--- server.c
+++ server.c
@@ -1,27 +1,123 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char** argv) {
+#define PORT "3490" // The Port users will be connecting to
+#define BACKLOG 10  // How  many pending connections queue will hold
 
-  int status;
-  struct addrinfo hints;
-  struct addrinfo *servinfo; // will point to the results
+void sigchild_handler(int s) {
+  (void)s; // quiet unused variable warning
 
-  memset(&hints, 9, sizeof hints); // make sure the struct is empty
-  hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
-  hints.ai_socktype = SOCK_STREAM; // TCP stream socket
-  hints.ai_flags = AI_PASSIVE; // Fill in my local IP for me
+  // waitpid() might overwrite errno, so we save and restore it:
+  int saved_errno = errno;
 
+  while (waitpid(-1, NULL, WNOHANG) > 0)
+    ;
 
-  if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
-    fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+  errno = saved_errno;
+}
+
+void *get_in_addr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+int main(int argc, char **argv) {
+
+  // listen on sock_fd, new connection on new_fd
+  int sockfd, new_fd;
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_add; // connector's address info
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes = 1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+
+    break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if(p == NULL) {
+    fprintf(stderr, "server: failed to bind\n");
     exit(1);
   }
 
-  freeaddrinfo(servinfo);
-  return 0;
+  if(listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  sa.sa_handler= sigchild_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if(sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+
+  printf("server: waiting for connections...\n");
+
+  while(1) { // main accept() loop
+    sin_size = sizeof their_add;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_add, &sin_size);
+
+    if(new_fd == -1) {
+      perror("accept");
+      continue;
+    }
+
+    inet_ntop(their_add.ss_family, get_in_addr((struct sockaddr *)&their_add), s, sizeof s);
+
+    if(!fork()) { // this is the child process
+      close(sockfd); // child doesn't need the listener
+      if(send(new_fd,"Hello World!", 13, 0) == -1)
+        perror("send");
+      close(new_fd);
+      exit(0);
+    }
+
+    close(new_fd); // parent doesn't need this
+  }
+  return 0; 
 }
